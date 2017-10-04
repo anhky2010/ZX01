725f957f5ed8a70cfac9271d6a395b4d959c798b
Ky2016z <anhky2010@gmail.com>
2017-10-04 17:07:01 +0700

    Fix Animation

---
+++ Assets/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+
+    public Transform player;
+    public Vector3 offset;
+    public float smoothSpeed;
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.position = player.position - offset;
+    }
+    float distance = 0;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //distance = this.transform.position.x - player.position.x;
+        //if (Mathf.Abs(distance) > 10)
+        //{
+        //    this.transform.position = Vector3.MoveTowards(this.transform.position, player.position - offset, smoothSpeed);
+        //}
+        this.transform.position = player.position - offset;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    public static PlayerController instance;
     private Animator animator;
     private GameObject Player;
+    bool isPressKey = false;
     // Use this for initialization
+    void Awake()
+    {
+        if (instance != null)
+        {
+            return;
+        }
+        instance = this;
+    }
     private void Start()
     {
-
-        animator = GetComponent<Animator>();
-        Player = GameObject.Find("Player");
-
-
+        animator = GetComponentInChildren<Animator>();
+        Player = instance.gameObject;
     }
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
         Attack();
         Move();
@@ -27,75 +33,88 @@ public class PlayerController : MonoBehaviour
     private float speed = 0;
     private void Move()
     {
-        
-        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_1") &&
-            !animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_2") &&
-            !animator.GetCurrentAnimatorStateInfo(0).IsName("Kick_1") &&
-            !animator.GetCurrentAnimatorStateInfo(0).IsName("Kick_2") &&
-            !animator.GetCurrentAnimatorStateInfo(0).IsName("Jumb"))
+        if (!isAnimationRunning())
         {
+            speed = Mathf.Clamp(speed, 0, 4);
             if (Input.GetKey(KeyCode.RightArrow))
             {
-                animator.SetFloat("Vel_x", speed += 0.05f);
-
+                animator.SetFloat("SpeedRunning", speed += 0.05f);
                 Player.transform.rotation = new Quaternion(0, 0, 0, 0);
-                Player.transform.Translate(Vector3.right * 10 * Time.deltaTime);
+                Player.transform.Translate(Vector3.right * speed * 4 * Time.deltaTime);
             }
             else if (Input.GetKey(KeyCode.LeftArrow))
             {
-                animator.SetFloat("Vel_x", speed += 0.05f); 
-
+                animator.SetFloat("SpeedRunning", speed += 0.05f);
                 Player.transform.rotation = new Quaternion(0, 180, 0, 0);
-                Player.transform.Translate(Vector3.right * 10 * Time.deltaTime);
+                Player.transform.Translate(Vector3.right * speed * 4 * Time.deltaTime);
             }
             else
             {
                 speed = 0;
-                animator.SetFloat("Vel_x", speed);
-               
+                animator.SetFloat("SpeedRunning", speed);
+
             }
+        }
+        else
+        {
+            speed = 0;
+            animator.SetFloat("SpeedRunning", speed);
+
         }
 
 
     }
     private void Attack()
     {
+        if (isAnimationRunning())
+        {
+            animator.ResetTrigger("param_Attack_0");
+            animator.ResetTrigger("param_Attack_1");
+            animator.ResetTrigger("param_Attack_2");
+            animator.ResetTrigger("param_Kick_1");
+            animator.ResetTrigger("param_Kick_2");
+            animator.ResetTrigger("param_Jump");
+        }
+
         if (Input.GetKeyDown(KeyCode.A))
         {
-            animator.SetBool("param_Attack_0", true);
+            animator.SetTrigger("param_Attack_0");
+
         }
-        else
         if (Input.GetKeyDown(KeyCode.S))
         {
-            animator.SetBool("param_Attack_1", true);
+            animator.SetTrigger("param_Attack_1");
         }
-        else if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            animator.SetBool("param_Attack_2", true);
+            animator.SetTrigger("param_Attack_2");
         }
-        else if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.Z))
         {
-            animator.SetBool("param_Kick_1", true);
+            animator.SetTrigger("param_Kick_1");
         }
-        else if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.X))
         {
-            animator.SetBool("param_Kick_2", true);
+            animator.SetTrigger("param_Kick_2");
         }
-        else if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            animator.SetBool("param_Jump", true);
+            animator.SetTrigger("param_Jump");
         }
-        else
-        {
 
-            animator.SetBool("param_Attack_0", false);
-            animator.SetBool("param_Attack_1", false);
-            animator.SetBool("param_Attack_2", false);
-            animator.SetBool("param_Jump", false);
-            animator.SetBool("param_Kick_1", false);
-            animator.SetBool("param_Kick_2", false);
-        }
 
     }
-
+    bool isAnimationRunning()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_1") ||
+           animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_2") ||
+           animator.GetCurrentAnimatorStateInfo(0).IsName("Attack_Normal") ||
+           animator.GetCurrentAnimatorStateInfo(0).IsName("Kick_1") ||
+           animator.GetCurrentAnimatorStateInfo(0).IsName("Kick_2") ||
+           animator.GetCurrentAnimatorStateInfo(0).IsName("Jumb"))
+        {
+            return true;
+        }
+        return false;
+    }
 }
